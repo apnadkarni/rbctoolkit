3f0f982a9c1995bc84e662a0ddddd25c2fbd59ea
techentin <techentin@3b297cb3-c71f-4973-8643-51e6ba862204>
2009-10-05 20:06:05 +0000

    Corrected and updated test files

--- trunk/rbc/tests/all.tcl
+++ trunk/rbc/tests/all.tcl
@@ -4,63 +4,17 @@
 # tests.  Execute it by invoking "source all.test" when running tcltest
 # in this directory.
 #
-# Copyright (c) 1998-2000 by Scriptics Corporation.
-# All rights reserved.
-# 
-# RCS: @(#) $Id: all.tcl,v 1.4 2004/07/04 22:04:20 patthoyts Exp $
 
-if {[lsearch [namespace children] ::tcltest] == -1} {
-    package require tcltest
-    namespace import ::tcltest::*
-}
+# restart using tclsh \
+exec tclsh "$0" "$@"
 
-set ::tcltest::testSingleFile false
-set ::tcltest::testsDirectory [file dir [info script]]
+package require tcltest 2
 
-# We need to ensure that the testsDirectory is absolute
-if {[catch {::tcltest::normalizePath ::tcltest::testsDirectory}]} {
-    # The version of tcltest we have here does not support
-    # 'normalizePath', so we have to do this on our own.
+package require Tk ;# This is the Tk test suite; fail early if no Tk!
 
-    set oldpwd [pwd]
-    catch {cd $::tcltest::testsDirectory}
-    set ::tcltest::testsDirectory [pwd]
-    cd $oldpwd
-}
+eval tcltest::configure $argv
+tcltest::configure -testdir [file normalize [file dirname [info script]]]
 
-set chan $::tcltest::outputChannel
-
-puts $chan "Tests running in interp:       [info nameofexecutable]"
-puts $chan "Tests running with pwd:        [pwd]"
-puts $chan "Tests running in working dir:  $::tcltest::testsDirectory"
-if {[llength $::tcltest::skip] > 0} {
-    puts $chan "Skipping tests that match:            $::tcltest::skip"
-}
-if {[llength $::tcltest::match] > 0} {
-    puts $chan "Only running tests that match:        $::tcltest::match"
-}
-
-if {[llength $::tcltest::skipFiles] > 0} {
-    puts $chan "Skipping test files that match:       $::tcltest::skipFiles"
-}
-if {[llength $::tcltest::matchFiles] > 0} {
-    puts $chan "Only sourcing test files that match:  $::tcltest::matchFiles"
-}
-
-set timeCmd {clock format [clock seconds]}
-puts $chan "Tests began at [eval $timeCmd]"
-
-# source each of the specified tests
-foreach file [lsort [::tcltest::getMatchingFiles]] {
-    set tail [file tail $file]
-    puts $chan $tail
-    if {[catch {source $file} msg]} {
-	puts $chan $msg
-    }
-}
-
-# cleanup
-puts $chan "\nTests ended at [eval $timeCmd]"
-::tcltest::cleanupTests 1
-return
+tcltest::configure -singleproc 1
+tcltest::runAllTests
 
