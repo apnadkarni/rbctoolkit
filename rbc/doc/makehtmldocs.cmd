96c6a0c57abd3924a7939220e3a6a1d11788da61
Ashok P. Nadkarni <apnmbx-wits@yahoo.com>
2018-01-18 18:06:50 +0530

    Added HTML docs

---
+++ rbc/doc/makehtmldocs.cmd
@@ -0,0 +1,4 @@
+mandoc -Thtml -Ostyle=rbc.css spline.n > spline.html
+mandoc -Thtml -Ostyle=rbc.css stripchart.n > stripchart.html
+mandoc -Thtml -Ostyle=rbc.css vector.n > vector.html
+mandoc -Thtml -Ostyle=rbc.css winop.n > winop.html
