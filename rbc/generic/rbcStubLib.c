c75ec696b7b34f77be4a59b449bdf39b7ba32fba
Ashok P. Nadkarni <apnmbx-wits@yahoo.com>
2018-02-02 11:59:12 +0530

    Install rpcStubLib.c so it can be used in lieu of the stubs library

--- rbc/generic/rbcStubLib.c
+++ rbc/generic/rbcStubLib.c
@@ -16,7 +16,8 @@
 #   define USE_TCL_STUBS
 #endif
 
-#include "rbc.h"
+#include "tcl.h"
+#include "rbcDecls.h"
 
 const RbcStubs *rbcStubsPtr;
 
@@ -47,7 +48,7 @@ Rbc_InitStubs(
     const char *result;
     void *data;
 
-    result = Tcl_PkgRequireEx(interp, PACKAGE_NAME, (CONST84 char *) version, exact, &data);
+    result = Tcl_PkgRequireEx(interp, "rbc", (CONST84 char *) version, exact, &data);
     if (!result || !data) {
         return NULL;
     }
