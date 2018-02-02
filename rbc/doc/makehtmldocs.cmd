9ffb5eb367c0afcad6a50b3f4c77092dbaa8bd2c
Ashok P. Nadkarni <apnmbx-wits@yahoo.com>
2018-02-02 09:42:36 +0530

    Added graph and barchart docs

--- rbc/doc/makehtmldocs.cmd
+++ rbc/doc/makehtmldocs.cmd
@@ -1,3 +1,5 @@
+mandoc -Thtml -Ostyle=rbc.css barchart.n > barchart.html
+mandoc -Thtml -Ostyle=rbc.css graph.n > graph.html
 mandoc -Thtml -Ostyle=rbc.css spline.n > spline.html
 mandoc -Thtml -Ostyle=rbc.css stripchart.n > stripchart.html
 mandoc -Thtml -Ostyle=rbc.css vector.n > vector.html
