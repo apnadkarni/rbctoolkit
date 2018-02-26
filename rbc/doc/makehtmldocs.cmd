6346f102c4d86c237bc4e0ac1f49151c4e91bd9b
Ashok P. Nadkarni <apnmbx-wits@yahoo.com>
2018-02-26 22:29:29 +0530

    Reformat HTML using groff

--- rbc/doc/makehtmldocs.cmd
+++ rbc/doc/makehtmldocs.cmd
@@ -1,3 +1,14 @@
+@echo INSTRUCTIONS:
+@echo.
+@echo On a Unix system, run the following commands.
+@echo   groff -mandoc -Thtml barchart.n etc.
+@echo Then edit the files to get rid of the horizontal __ lines
+@echo Just before the Name and at the end of SYNOPSIS.
+
+@rem the commands below produce badly styled output, at least with
+@rem the mandoc program available for Windows, thus the above instruction.
+@exit /b
+
 mandoc -Thtml -Ostyle=rbc.css barchart.n > barchart.html
 mandoc -Thtml -Ostyle=rbc.css graph.n > graph.html
 mandoc -Thtml -Ostyle=rbc.css spline.n > spline.html
